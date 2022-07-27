54ba063abd059fd2e2514a3efa66351b3f0e0589
Diksha Magotra <dikshamagotra2021@gmail.com>
2022-07-27 17:25:51 -0400

    Updated Code with Comments

--- ACC search engine/Search-Engine-master/src/HTMLtoText.java
+++ ACC search engine/Search-Engine-master/src/HTMLtoText.java
@@ -1,113 +1,116 @@
-import java.io.*;  
-import javax.swing.text.html.*;
-import javax.swing.text.html.parser.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.parser.ParserDelegator;
 
 import org.jsoup.Jsoup;
 
-import java.net.*;
-
 public class HTMLtoText extends HTMLEditorKit.ParserCallback {
- StringBuffer s;
+	StringBuffer s; // string buffer class is used for creating mutable strings
 
- public HTMLtoText() {}
+	public HTMLtoText() {
+	}
 
- public void parse(Reader in) throws IOException {
-   s = new StringBuffer();
-   ParserDelegator delegator = new ParserDelegator();
-   // the third parameter is TRUE to ignore charset directive
-   delegator.parse(in, this, Boolean.TRUE);
- }
+	// Reader class is an abstract class that represent a stream of characters
+	public void parse(Reader in) throws IOException {
+		s = new StringBuffer();
+		ParserDelegator delegator = new ParserDelegator();
+		// the third parameter is TRUE to ignore charset directive
+		delegator.parse(in, this, Boolean.TRUE);
+	}
 
- public void handleText(char[] text, int pos) {
-   s.append(text);
- }
+	public void handleText(char[] text, int pos) {
+		s.append(text);
+	}
 
- public String getText() {
-   return s.toString();
- }
+	public String getText() {
+		return s.toString(); // converts string object into a string
+	}
 
- public static void main(String[] args) throws IOException,FileNotFoundException,NullPointerException {
+	public static void conversion(File destinationfile, int m)
+			throws IOException, FileNotFoundException, NullPointerException
 
-	 
+	{
 
-	 int m = 1;
+		try {
 
-	 try {
+			org.jsoup.nodes.Document _doc = Jsoup.parse(destinationfile, "UTF-8");
 
-	 File directoryPth = new File("/Users/dikshamagotra/Downloads/ACC search engine/Search-Engine-master/W3C Web Pages");
+			BufferedWriter bufferedWriterBW = new BufferedWriter(new FileWriter(
+					"/Users/dikshamagotra/Downloads/ACC search engine/Search-Engine-master/W3C Web Pages/Text"
+							+ destinationfile.getName() + ".txt"));
 
-	 File[] FilesArrayA1 = directoryPth.listFiles();
-	 System.out.println(FilesArrayA1.length);
+			bufferedWriterBW.write(_doc.text());
 
-	 File[] FilesArrayA2 = new File[103];
+			bufferedWriterBW.close();
 
-	 int totalCount=0;
+			System.out.println("File " + destinationfile.getName() + " to  " + destinationfile.getName() + ".txt");
 
-	 for(int i=0;i<FilesArrayA1.length;i++)
+		} catch (Exception e) {
 
-	 {
+		}
 
-		 if(FilesArrayA1[i].isFile())
+	}
 
-	 {
+	public static void main(String[] args) throws IOException, FileNotFoundException, NullPointerException {
 
-	 FilesArrayA2[totalCount] = FilesArrayA1[i];
+		int m = 1;
 
-	 totalCount++;
+		try {
 
-	 }
+			File directoryPth = new File(
+					"/Users/dikshamagotra/Downloads/ACC search engine/Search-Engine-master/W3C Web Pages");
 
-	 }
+			File[] FilesArrayA1 = directoryPth.listFiles();
+			System.out.println(FilesArrayA1.length);
 
-	 for(int i=0;i<104;i++)
+			File[] FilesArrayA2 = new File[105];
 
-	 {
+			int totalCount = 0;
 
-	 conversion(FilesArrayA2[i],m);
+			for (int i = 0; i < FilesArrayA1.length; i++)
 
-	 m = m + 1;
+			{
 
-	 }
+				if (FilesArrayA1[i].isFile())
 
-	 }
+				{
 
-	  catch (Exception e) {
+					FilesArrayA2[totalCount] = FilesArrayA1[i];
 
-	 System.out.println("Exception:"+e);
+					totalCount++;
 
-	 }
+				}
 
-	 finally
+			}
 
-	 {
+			for (int i = 0; i < 104; i++)
 
-	 }
+			{
 
-	  
+				conversion(FilesArrayA2[i], m);
 
-	 }
+				m = m + 1;
+			}
+		}
 
-	  
+		catch (Exception e) {
 
-	 public static void conversion(File destinationfile,int m) throws IOException,FileNotFoundException,NullPointerException
+			System.out.println("Exception:" + e);
 
-	 {
+		}
 
-	 try {
+		finally
 
-	 org.jsoup.nodes.Document _doc = Jsoup.parse(destinationfile, "UTF-8");
+		{
 
-	 BufferedWriter bufferedWriterBW = new BufferedWriter(new FileWriter("/Users/dikshamagotra/Downloads/ACC search engine/Search-Engine-master/W3C Web Pages"+destinationfile.getName()+".txt"));
+		}
 
-	 bufferedWriterBW.write(_doc.text());
+	}
 
-	 bufferedWriterBW.close();
-
-	 System.out.println("File "+destinationfile.getName()+" to  "+destinationfile.getName()+".txt");
-
-	 } catch (Exception e) {
-
-	 }
-
-}
 }
